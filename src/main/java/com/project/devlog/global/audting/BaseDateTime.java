e6820423ad6b95784cfa792d64755f6cef46183d
ahj30420 <ahj30420@naver.com>
2026-05-16 15:08:23 +0900

    feat: JPA Auditing 기반 공통 생성/수정 시간 엔티티 추가

---
+++ src/main/java/com/project/devlog/global/audting/BaseDateTime.java
@@ -0,0 +1,24 @@
+package com.project.devlog.global.audting;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+public abstract class BaseDateTime {
+
+    @CreatedDate
+    @Column(updatable = false)
+    protected LocalDateTime createDateTime;
+
+    @LastModifiedDate
+    protected LocalDateTime lastModifiedDateTime;
+
+}
