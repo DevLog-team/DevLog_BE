2a8672d0e2eae71e170dd0dd931534758a29936f
ahj30420 <ahj30420@naver.com>
2026-05-16 15:08:04 +0900

    feat: 전역 예외 처리 및 공통 에러 코드 구조 추가

---
+++ src/main/java/com/project/devlog/global/exception/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,40 @@
+package com.project.devlog.global.exception.advice;
+
+import com.project.devlog.global.exception.BusinessException;
+import com.project.devlog.global.exception.errorcode.CommonErrorCode;
+import com.project.devlog.global.response.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+
+    @ExceptionHandler
+    public ResponseEntity<Object> BusinessException(final BusinessException e) {
+        log.error("[BusinessException] {} - {}", e.getHttpStatus().value(), e.getMessage());
+        return ResponseEntity.status(e.getHttpStatus())
+                .body(new ErrorResponse(e.getHttpStatus(), e.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> exception(final Exception e) {
+        HttpStatus errorStatus = CommonErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus();
+        log.error("[{}] {}", e.getClass().getName(), e.getMessage());
+        return ResponseEntity.status(errorStatus)
+                .body(new ErrorResponse(errorStatus, e.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handleValidationException(final MethodArgumentNotValidException e) {
+        HttpStatus errorStatus = HttpStatus.BAD_REQUEST;
+        String errorMessage = e.getBindingResult().getAllErrors().getFirst().getDefaultMessage();
+        log.error("[MethodArgumentNotValidException] {}", errorMessage);
+        return ResponseEntity.status(errorStatus)
+                .body(new ErrorResponse(errorStatus, errorMessage));
+    }
+}
