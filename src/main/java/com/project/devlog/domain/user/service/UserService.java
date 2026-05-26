4db88fb84459b04c8dc3b43f99bf923c3c7741e6
ahj30420 <ahj30420@naver.com>
2026-05-26 18:59:03 +0900

    [#11] refactor: 회원가입 엔티티 변환 로직을 UserMapper로 이관

--- src/main/java/com/project/devlog/domain/user/service/UserService.java
+++ src/main/java/com/project/devlog/domain/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.project.devlog.domain.user.service;
 import com.project.devlog.domain.user.dto.request.SignupRequest;
 import com.project.devlog.domain.user.entity.User;
 import com.project.devlog.domain.user.entity.enums.UserRole;
+import com.project.devlog.domain.user.mapper.UserMapper;
 import com.project.devlog.domain.user.repository.UserRepository;
 import com.project.devlog.global.exception.BusinessException;
 import com.project.devlog.global.exception.errorcode.UserErrorCode;
@@ -19,18 +20,14 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserMapper userMapper;
 
     @Transactional
     public Long signup(@Valid SignupRequest requestDto) {
         validateDuplicateEmail(requestDto.email());
 
         String encodedPassword = passwordEncoder.encode(requestDto.password());
-        User user = User.builder()
-                .email(requestDto.email())
-                .password(encodedPassword)
-                .name(requestDto.name())
-                .role(UserRole.USER)
-                .build();
+        User user = userMapper.toUser(requestDto, encodedPassword);
 
         return userRepository.save(user).getId();
     }
