efd4e115804f8f4eb9284da89dce4533ec188f57
ahj30420 <ahj30420@naver.com>
2026-06-03 17:31:16 +0900

    [#12] feat: Task(작업) 관련 엔티티 추가

---
+++ src/main/java/com/project/devlog/domain/task/entity/Task.java
@@ -0,0 +1,80 @@
+package com.project.devlog.domain.task.entity;
+
+import com.project.devlog.domain.project.entity.Project;
+import com.project.devlog.domain.task.entity.enums.TaskPriority;
+import com.project.devlog.domain.task.entity.enums.TaskStatus;
+import com.project.devlog.domain.user.entity.User;
+import com.project.devlog.global.audting.BaseDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "tasks")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Task extends BaseDateTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TaskPriority priority;
+
+    private LocalDate dueDate;
+
+    private LocalDateTime completedAt;
+
+    private boolean isDeleted = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @OneToMany(mappedBy = "task")
+    private List<TaskTag> tags;
+
+    @Builder
+    private Task(Long id, String title, String description, TaskStatus status, TaskPriority priority, LocalDate dueDate,
+                User user, Project project) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.user = user;
+        this.project = project;
+    }
+}
